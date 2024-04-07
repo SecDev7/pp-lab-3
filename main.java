6a5ff61d22cb452e41958c64510536ec9c0dbce0
Sebastian Skaba <162018809+SecDev7@users.noreply.github.com>
2024-04-07 11:16:17 +0200

    Add files via upload

---
+++ main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        point point = new point(6.3, 5.4);
+        System.out.println("wspolrzedne punktu: x = " + point.getX() + ", y = " + point.getY());
+    }
+}
